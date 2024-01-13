a248560cdac9d11998f66bd0e3a81385898f1310
Vencoca <vaclav.kesler@gmail.com>
2024-01-13 15:18:21 +0100

    Material UI init

--- app/(public)/layout.tsx
+++ app/(public)/layout.tsx
@@ -1,14 +1,25 @@
 import type { Metadata } from 'next'
 import '@/app/globals.css'
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
+import Head from 'next/head';
+import { CssBaseline } from '@mui/material';
+
 
 export const metadata: Metadata = {
   title: 'BMS',
   description: 'Building managment system',
 }
 
-export default function RootLayout({children,}: {children: React.ReactNode}) {
+export default function RootLayout({ children, }: { children: React.ReactNode }) {
   return (
     <html lang="en">
+      <Head>
+        <meta name="viewport" content="initial-scale=1, width=device-width" />
+      </Head>
+      <CssBaseline />
       <body>{children}</body>
     </html>
   )
--- app/(public)/page.tsx
+++ app/(public)/page.tsx
@@ -1,17 +1,8 @@
-import Sidebar from "@/components/sidebar"
+import Sidebar from "@/components/sidebar";
+
 
 export default function Home() {
   return (
-    <main className="flex">
-      <Sidebar>
-        <h2 className="text-4xl ">This is a sidebar</h2>
-        <p>Option one</p>
-        <p>Option two</p>
-      </Sidebar>
-      <div className="w-full grid place-content-center">
-        <h1 className="text-5xl">This is the homepage of BMS</h1>
-      </div>
-
-    </main>
+   <Sidebar></Sidebar>
   )
 }

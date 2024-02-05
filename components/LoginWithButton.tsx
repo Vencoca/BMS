ba85f1b911c7626e12e8066e5c8dfbc05047f8eb
Vencoca <vaclav.kesler@gmail.com>
2024-02-05 22:26:42 +0100

    Prevent default on button

--- components/LoginWithButton.tsx
+++ components/LoginWithButton.tsx
@@ -11,7 +11,7 @@ interface LoginWithButtonProps {
 
 export default function LoginWithButton({ provider, callbackUrl, text, imageSrc }: LoginWithButtonProps) {
     return (
-        <Button onClick={() => signIn(provider, { callbackUrl })} variant="contained" href="#contained-buttons">
+        <Button onClick={(e) => {e.preventDefault();signIn(provider, { callbackUrl });}} variant="contained" href="#contained-buttons">
             <Image width={16} height={16} style={{marginRight:'8px'}} src={imageSrc} alt={`${text} icon`}></Image>
             {text}
         </Button>
